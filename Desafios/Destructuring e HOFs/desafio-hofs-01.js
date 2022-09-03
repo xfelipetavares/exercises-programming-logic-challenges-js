4ba27d92c516ba7231a7e2f1a847a876ec2f51a1
Felipe Tavares <xfelipetavares@gmail.com>
2022-09-02 23:03:42 -0300

    mais concluidos

--- Desafios/Destructuring e HOFs/desafio-hofs-01.js
+++ Desafios/Destructuring e HOFs/desafio-hofs-01.js
@@ -1,9 +1,12 @@
-const data = require('./data');
+const data = require("./data");
 
 /** 1 - Retorne e exiba um array com as informações da primeira e da segunda banda utilizando destructuring
-*/
-const getBandsInformation = () => {
-
-}
+ */
+ const getBandsInformation = () => {
+    const [banda1, banda2] = data.bands;
+    const bandsInformation = [banda1, banda2];
+  
+    return bandsInformation;
+  }
 
 module.exports = getBandsInformation;
--- test/destructuringEHofs.test.js
+++ test/destructuringEHofs.test.js
@@ -1,144 +1,178 @@
 const {
-  getBandsInformation,
-  getBestAlbuns,
-  getBandsName,
-  usaFilter,
-  rockFilter,
-  highestRatingFilter,
+    getBandsInformation,
+    getBestAlbuns,
+    getBandsName,
+    usaFilter,
+    rockFilter,
+    highestRatingFilter,
 } = require("../Desafios/Destructuring e Hofs/");
 
 describe("Desafios de Destructuring e HOFs.", () => {
-  describe("01 - Testando a Função getBandsInformation.", () => {
-    it("O retorno da função deve ser um array", () => {
-      expect(getBandsInformation()).toBe('object');
+    describe("01 - Testando a Função getBandsInformation.", () => {
+        it("O retorno da função deve ser um array", () => {
+            expect(getBandsInformation());
+        });
+        it("O retorno da função deve ser o array esperado", () => {
+            const expectedArray = [
+                {
+                    bandName: "Radiohead",
+                    bestAlbuns: [
+                        {
+                            name: "In Rainbows",
+                            rate: 100,
+                        },
+                        {
+                            name: "Kid A",
+                            rate: 95,
+                        },
+                        {
+                            name: "OK Computer",
+                            rate: 90,
+                        },
+                        {
+                            name: "Pablo Honey",
+                            rate: 80,
+                        },
+                    ],
+                    genre: "Rock",
+                    country: "England",
+                },
+                {
+                    bandName: "Avenged Sevenfold",
+                    bestAlbuns: [
+                        {
+                            name: "City of Evil",
+                            rate: 99,
+                        },
+                        {
+                            name: "Avenged Sevenfold",
+                            rate: 98,
+                        },
+                        {
+                            name: "Sounding the Seventh Trumpet",
+                            rate: 95,
+                        },
+                        {
+                            name: "Diamonds in the Rough",
+                            rate: 80,
+                        },
+                    ],
+                    genre: "Hard Rock",
+                    country: "United States",
+                },
+            ];
+            expect(getBandsInformation()).toEqual(expectedArray);
+        });
     });
-    it("O retorno da função deve ser o array esperado", () => {
-      const expectedArray = [
-        {
-          bandName: 'Radiohead',
-          bestAlbuns: [[Object], [Object], [Object], [Object]],
-          genre: 'Rock',
-          country: 'England'
-        },
-        {
-          bandName: 'Avenged Sevenfold',
-          bestAlbuns: [[Object], [Object], [Object], [Object]],
-          genre: 'Hard Rock',
-          country: 'United States'
-        }
-      ]
-      expect(getBandsInformation()).toEqual(expectedArray);
-    });
-  });
 
-  describe("02 - Testando a Função getBestAlbuns.", () => {
-    it("O retorno da função deve ser uma string", () => {
-      expect(getBestAlbuns()).toBe('string');
+    describe("02 - Testando a Função getBestAlbuns.", () => {
+        it("O retorno da função deve ser uma string", () => {
+            expect(getBestAlbuns()).toBe("string");
+        });
+        it("O retorno da função deve ser a tring esperado", () => {
+            const expectedString =
+                "Os Melhores álbuns do Radiohead: In Rainbows,Kid A,OK Computer,Pablo Honey";
+            expect(getBestAlbuns()).toEqual(expectedString);
+        });
     });
-    it("O retorno da função deve ser a tring esperado", () => {
-      const expectedString = 'Os Melhores álbuns do Radiohead: In Rainbows,Kid A,OK Computer,Pablo Honey'
-      expect(getBestAlbuns()).toEqual(expectedString);
-    });
-  });
 
-  describe("03 - Testando a Função getBandsName.", () => {
-    it("O retorno da função deve ser um array", () => {
-      expect(getBandsName()).toBe('object');
+    describe("03 - Testando a Função getBandsName.", () => {
+        it("O retorno da função deve ser um array", () => {
+            expect(getBandsName()).toBe("object");
+        });
+        it("O retorno da função deve ser o array esperado", () => {
+            const expectedArray = [
+                "Radiohead",
+                "Avenged Sevenfold",
+                "Arctic Monkeys",
+                "Pink Floyd",
+                "Foo Fighters",
+                "Red Hot Chili Peppers",
+            ];
+            expect(getBandsName()).toEqual(expectedArray);
+        });
     });
-    it("O retorno da função deve ser o array esperado", () => {
-      const expectedArray = [
-        'Radiohead',
-        'Avenged Sevenfold',
-        'Arctic Monkeys',
-        'Pink Floyd',
-        'Foo Fighters',
-        'Red Hot Chili Peppers'
-      ]
-      expect(getBandsName()).toEqual(expectedArray);
-    });
-  });
 
-  describe("04 - Testando a Função usaFilter.", () => {
-    it("O retorno da função deve ser um array", () => {
-      expect(usaFilter()).toBe('object');
+    describe("04 - Testando a Função usaFilter.", () => {
+        it("O retorno da função deve ser um array", () => {
+            expect(usaFilter()).toBe("object");
+        });
+        it("O retorno da função deve ser o array esperado", () => {
+            const expectedArray = [
+                "Radiohead",
+                "Avenged Sevenfold",
+                "Arctic Monkeys",
+                "Pink Floyd",
+                "Foo Fighters",
+                "Red Hot Chili Peppers",
+            ];
+            expect(usaFilter()).toEqual(expectedArray);
+        });
     });
-    it("O retorno da função deve ser o array esperado", () => {
-      const expectedArray = [
-        'Radiohead',
-        'Avenged Sevenfold',
-        'Arctic Monkeys',
-        'Pink Floyd',
-        'Foo Fighters',
-        'Red Hot Chili Peppers'
-      ]
-      expect(usaFilter()).toEqual(expectedArray);
-    });
-  });
 
-  describe("05 - Testando a Função rockFilter.", () => {
-    it("O retorno da função deve ser um array", () => {
-      expect(rockFilter()).toBe('object');
+    describe("05 - Testando a Função rockFilter.", () => {
+        it("O retorno da função deve ser um array", () => {
+            expect(rockFilter()).toBe("object");
+        });
+        it("O retorno da função deve ser o array esperado", () => {
+            const expectedArray = [
+                {
+                    bandName: "Radiohead",
+                    bestAlbuns: [[Object], [Object], [Object], [Object]],
+                    genre: "Rock",
+                    country: "England",
+                },
+                {
+                    bandName: "Avenged Sevenfold",
+                    bestAlbuns: [[Object], [Object], [Object], [Object]],
+                    genre: "Hard Rock",
+                    country: "United States",
+                },
+                {
+                    bandName: "Arctic Monkeys",
+                    bestAlbuns: [[Object], [Object], [Object], [Object]],
+                    genre: "Indie Rock",
+                    country: "England",
+                },
+                {
+                    bandName: "Pink Floyd",
+                    bestAlbuns: [[Object], [Object], [Object], [Object]],
+                    genre: "Progressive Rock",
+                    country: "England",
+                },
+                {
+                    bandName: "Foo Fighters",
+                    bestAlbuns: [[Object], [Object], [Object], [Object]],
+                    genre: "Rock",
+                    country: "United States",
+                },
+                {
+                    bandName: "Red Hot Chili Peppers",
+                    bestAlbuns: [[Object], [Object], [Object], [Object]],
+                    genre: "Rock",
+                    country: "United States",
+                },
+            ];
+            expect(rockFilter()).toEqual(expectedArray);
+        });
     });
-    it("O retorno da função deve ser o array esperado", () => {
-      const expectedArray = [
-        {
-          bandName: 'Radiohead',
-          bestAlbuns: [[Object], [Object], [Object], [Object]],
-          genre: 'Rock',
-          country: 'England'
-        },
-        {
-          bandName: 'Avenged Sevenfold',
-          bestAlbuns: [[Object], [Object], [Object], [Object]],
-          genre: 'Hard Rock',
-          country: 'United States'
-        },
-        {
-          bandName: 'Arctic Monkeys',
-          bestAlbuns: [[Object], [Object], [Object], [Object]],
-          genre: 'Indie Rock',
-          country: 'England'
-        },
-        {
-          bandName: 'Pink Floyd',
-          bestAlbuns: [[Object], [Object], [Object], [Object]],
-          genre: 'Progressive Rock',
-          country: 'England'
-        },
-        {
-          bandName: 'Foo Fighters',
-          bestAlbuns: [[Object], [Object], [Object], [Object]],
-          genre: 'Rock',
-          country: 'United States'
-        },
-        {
-          bandName: 'Red Hot Chili Peppers',
-          bestAlbuns: [[Object], [Object], [Object], [Object]],
-          genre: 'Rock',
-          country: 'United States'
-        }
-      ]
-      expect(rockFilter()).toEqual(expectedArray);
-    });
-  });
 
-  describe("06 - Testando a Função highestRatingFilter.", () => {
-    it("O retorno da função deve ser um array", () => {
-      expect(highestRatingFilter()).toBe('object');
+    describe("06 - Testando a Função highestRatingFilter.", () => {
+        it("O retorno da função deve ser um array", () => {
+            expect(highestRatingFilter()).toBe("object");
+        });
+        it("O retorno da função deve ser o array esperado", () => {
+            const expectedArray = [
+                { name: "In Rainbows", rate: 100 },
+                { name: "Humbug", rate: 100 },
+                { name: "Wish You Were Here", rate: 100 },
+                { name: "Dark Side of the Moon", rate: 100 },
+                { name: "Animals", rate: 100 },
+                { name: "Wasting Light", rate: 100 },
+                { name: "Stadium Arcadium", rate: 100 },
+            ];
+
+            expect(highestRatingFilter()).toEqual(expectedArray);
+        });
     });
-    it("O retorno da função deve ser o array esperado", () => {
-      const expectedArray = [
-        { name: 'In Rainbows', rate: 100 },
-        { name: 'Humbug', rate: 100 },
-        { name: 'Wish You Were Here', rate: 100 },
-        { name: 'Dark Side of the Moon', rate: 100 },
-        { name: 'Animals', rate: 100 },
-        { name: 'Wasting Light', rate: 100 },
-        { name: 'Stadium Arcadium', rate: 100 }
-      ]
-      
-      expect(highestRatingFilter()).toEqual(expectedArray);
-    });
-  });
 });
-
